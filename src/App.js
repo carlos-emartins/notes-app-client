34ecd06cd839a0301779585f8788d36795b05b3e
carlos-emartins <cedsm.snf20@uea.edu.br>
2022-03-04 10:32:01 -0400

    Finished

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import NoteContainer from './NoteContainer'
+
+import './App.css'
+
+function App() {
+  const [allNotes, setAllNotes] = useState([])
+
+  const getAllNotes = () => {
+    axios.get('http://localhost:3001/notes').then((res) => {
+      setAllNotes(res.data)
+    })
+  }
+
+  useEffect(() => {
+    getAllNotes()
+  }, [])
+
+  const addNotes = () => {
+    axios
+      .post('http://localhost:3001/notes', {
+        body: '# Hello World!',
+      })
+      .then(() => {
+        getAllNotes()
+      })
+  }
+
+  const createNoteContainer = () => {
+    return allNotes.map((note) => (
+      <NoteContainer note={note} getAllNotes={() => getAllNotes()} />
+    ))
+  }
+
+  return (
+    <div>
+      <h1>📒 Notes</h1>
+
+      <button type="button" onClick={addNotes}>
+        Add New Note
+      </button>
+
+      <br />
+      <br />
+
+      {createNoteContainer()}
+    </div>
+  )
+}
+
+export default App
---
+++ src/NoteContainer.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+
+import axios from 'axios'
+import ReactMarkdown from 'react-markdown'
+
+function NoteContainer({ note, getAllNotes }) {
+  const [newBody, setNewBody] = useState('')
+  const [edit, setEdit] = useState(false)
+
+  const onToggle = () => {
+    setEdit(!edit)
+  }
+
+  const updateNote = (id, newBody) => {
+    axios
+      .put(`http://localhost:3001/notes/${id}`, {
+        newBody: newBody,
+      })
+      .then(() => {
+        onToggle()
+        getAllNotes()
+      })
+  }
+
+  const deleteNote = (id) => {
+    axios.delete(`http://localhost:3001/notes/${id}`).then(() => {
+      getAllNotes()
+    })
+  }
+
+  return (
+    <div className="Container">
+      <div className="NoteHeader">
+        <span>{note.createdAt}</span>
+
+        <div>
+          {edit ? (
+            <span
+              className="ButtonSpan"
+              role="img"
+              aria-label="preview"
+              onClick={() => updateNote(note.id, newBody)}
+            >
+              👌
+            </span>
+          ) : (
+            <span
+              className="ButtonSpan"
+              role="img"
+              aria-label="preview"
+              onClick={() => onToggle()}
+            >
+              ✍️
+            </span>
+          )}
+
+          <span
+            className="ButtonSpan"
+            role="img"
+            aria-label="Delete"
+            onClick={() => deleteNote(note.id)}
+          >
+            🗑️
+          </span>
+        </div>
+      </div>
+
+      {edit ? (
+        <textarea
+          className="StyledTextarea"
+          defaultValue={note.body}
+          onChange={(value) => setNewBody(value.target.value)}
+        />
+      ) : (
+        <div className="NotePreviewBody">
+          <ReactMarkdown children={note.body} />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default NoteContainer
